018bd0a523638ee5e763de660b7e39f59e1b8292
Balázs Mátyássy <balazs.matyassy@hotmail.com>
2024-05-30 07:39:22 +0200

    Initial commit

---
+++ 02-list/weather.js
@@ -0,0 +1,514 @@
+const measurements = [
+    {
+      "measurement_id": "0903021d-6932-484d-a7b1-a6591ec8895f",
+      "date": "2024-01-17",
+      "city": "Győr",
+      "temperature": 2,
+      "rain": 0,
+      "wind": 0
+    },
+    {
+      "measurement_id": "fcf9cd49-72ce-46e6-8583-9c6800c15091",
+      "date": "2024-01-21",
+      "city": "Budapest",
+      "temperature": 6,
+      "rain": 0,
+      "wind": 0
+    },
+    {
+      "measurement_id": "ce6073a2-500a-492d-9228-2adc09a568c3",
+      "date": "2024-01-21",
+      "city": "Győr",
+      "temperature": 5,
+      "rain": 0,
+      "wind": 0
+    },
+    {
+      "measurement_id": "6586b5fc-9cc0-41ed-bf55-97f88354462b",
+      "date": "2024-01-30",
+      "city": "Debrecen",
+      "temperature": 10,
+      "rain": 0,
+      "wind": 0
+    },
+    {
+      "measurement_id": "2ee2165d-059e-4c04-af4c-85348d456083",
+      "date": "2024-01-21",
+      "city": "Budapest",
+      "temperature": 7,
+      "rain": 25,
+      "wind": 0
+    },
+    {
+      "measurement_id": "2ed86da5-da15-4ebb-ae6a-cc21c556d3cd",
+      "date": "2024-01-03",
+      "city": "Szeged",
+      "temperature": 10,
+      "rain": 0,
+      "wind": 39
+    },
+    {
+      "measurement_id": "390fae31-1e52-423a-b7d4-02d7c4871aa2",
+      "date": "2024-01-18",
+      "city": "Budapest",
+      "temperature": 5,
+      "rain": 0,
+      "wind": 29
+    },
+    {
+      "measurement_id": "3f33cc7d-eaa3-4020-9d0b-057ba2bfe4ee",
+      "date": "2024-01-16",
+      "city": "Budapest",
+      "temperature": 9,
+      "rain": 0,
+      "wind": 0
+    },
+    {
+      "measurement_id": "7892373d-89dd-4be5-9d76-39ba8ae8d8c8",
+      "date": "2024-01-11",
+      "city": "Szeged",
+      "temperature": 6,
+      "rain": 1,
+      "wind": 0
+    },
+    {
+      "measurement_id": "c5848f3d-516c-477b-93dd-b715cff82e6f",
+      "date": "2024-01-29",
+      "city": "Debrecen",
+      "temperature": 7,
+      "rain": 48,
+      "wind": 0
+    },
+    {
+      "measurement_id": "a84ec74b-ad1a-4c84-a5b6-ec7a02eea862",
+      "date": "2024-01-10",
+      "city": "Debrecen",
+      "temperature": 5,
+      "rain": 0,
+      "wind": 46
+    },
+    {
+      "measurement_id": "87b70978-31fa-40d8-bf3c-a097f7f80d99",
+      "date": "2024-01-24",
+      "city": "Szeged",
+      "temperature": 7,
+      "rain": 0,
+      "wind": 0
+    },
+    {
+      "measurement_id": "2738d974-055f-420c-815b-ebfd94587c5d",
+      "date": "2024-01-30",
+      "city": "Győr",
+      "temperature": 1,
+      "rain": 4,
+      "wind": 28
+    },
+    {
+      "measurement_id": "dedd2b1a-b1f9-426f-ba4a-0985c69a299c",
+      "date": "2024-01-29",
+      "city": "Budapest",
+      "temperature": 8,
+      "rain": 0,
+      "wind": 2
+    },
+    {
+      "measurement_id": "d8171a1c-742d-4aa9-8163-3ee8a3bfba54",
+      "date": "2024-01-20",
+      "city": "Szeged",
+      "temperature": 4,
+      "rain": 11,
+      "wind": 0
+    },
+    {
+      "measurement_id": "ca4660c7-6cd9-4556-9ff4-c7d9181233ea",
+      "date": "2024-01-23",
+      "city": "Győr",
+      "temperature": 8,
+      "rain": 0,
+      "wind": 0
+    },
+    {
+      "measurement_id": "25b1b8c2-d5de-4f01-8323-db851808c78e",
+      "date": "2024-01-03",
+      "city": "Budapest",
+      "temperature": 9,
+      "rain": 0,
+      "wind": 30
+    },
+    {
+      "measurement_id": "115b9f7b-f82e-4d0f-96bb-3b4b1af5a671",
+      "date": "2024-01-26",
+      "city": "Budapest",
+      "temperature": 9,
+      "rain": 0,
+      "wind": 0
+    },
+    {
+      "measurement_id": "425976cf-1be3-460b-9e32-d1e9aaff086e",
+      "date": "2024-01-11",
+      "city": "Budapest",
+      "temperature": 6,
+      "rain": 0,
+      "wind": 0
+    },
+    {
+      "measurement_id": "1a34842d-8331-4175-9464-02dd2f560f89",
+      "date": "2024-01-07",
+      "city": "Budapest",
+      "temperature": 7,
+      "rain": 0,
+      "wind": 38
+    },
+    {
+      "measurement_id": "0ce9bf0d-8b11-456e-886a-8106736ea494",
+      "date": "2024-01-04",
+      "city": "Budapest",
+      "temperature": 6,
+      "rain": 0,
+      "wind": 11
+    },
+    {
+      "measurement_id": "3eb6d6ed-11c1-4f7b-9ec0-7738e42afdc5",
+      "date": "2024-01-29",
+      "city": "Győr",
+      "temperature": 5,
+      "rain": 16,
+      "wind": 0
+    },
+    {
+      "measurement_id": "bcf41e07-bf09-46c6-95c9-891f951591d4",
+      "date": "2024-01-28",
+      "city": "Budapest",
+      "temperature": 1,
+      "rain": 7,
+      "wind": 0
+    },
+    {
+      "measurement_id": "708b6a67-123e-4b8f-87ac-c4c3a57ad2c7",
+      "date": "2024-01-26",
+      "city": "Szeged",
+      "temperature": 6,
+      "rain": 47,
+      "wind": 0
+    },
+    {
+      "measurement_id": "155832bd-f6a8-4947-9a78-95b439e70106",
+      "date": "2024-01-11",
+      "city": "Budapest",
+      "temperature": 8,
+      "rain": 37,
+      "wind": 40
+    },
+    {
+      "measurement_id": "ccd2a20e-f219-499e-9528-915ea8ed4f5a",
+      "date": "2024-01-03",
+      "city": "Debrecen",
+      "temperature": 2,
+      "rain": 0,
+      "wind": 0
+    },
+    {
+      "measurement_id": "16062443-d7b1-4941-bd8e-12518694babd",
+      "date": "2024-01-01",
+      "city": "Budapest",
+      "temperature": 3,
+      "rain": 0,
+      "wind": 48
+    },
+    {
+      "measurement_id": "2597ba7a-fddf-473c-8762-518d1f0e8b1c",
+      "date": "2024-01-30",
+      "city": "Budapest",
+      "temperature": 6,
+      "rain": 19,
+      "wind": 17
+    },
+    {
+      "measurement_id": "cf5f7621-d93b-4b37-b6ba-db54c8adaee7",
+      "date": "2024-01-01",
+      "city": "Budapest",
+      "temperature": 8,
+      "rain": 1,
+      "wind": 0
+    },
+    {
+      "measurement_id": "4f7a9c46-4794-4a71-8b42-107e06853484",
+      "date": "2024-01-04",
+      "city": "Debrecen",
+      "temperature": 8,
+      "rain": 46,
+      "wind": 24
+    },
+    {
+      "measurement_id": "ecc4ba51-2c1b-4ff9-8da6-d03528e0bdae",
+      "date": "2024-01-07",
+      "city": "Győr",
+      "temperature": 1,
+      "rain": 41,
+      "wind": 0
+    },
+    {
+      "measurement_id": "bb689c30-bc1e-48c7-84ed-79431c6e8807",
+      "date": "2024-01-03",
+      "city": "Debrecen",
+      "temperature": 7,
+      "rain": 0,
+      "wind": 0
+    },
+    {
+      "measurement_id": "79aca250-4d65-4efb-8485-e30885e8668f",
+      "date": "2024-01-18",
+      "city": "Budapest",
+      "temperature": 10,
+      "rain": 0,
+      "wind": 45
+    },
+    {
+      "measurement_id": "0db4a234-ccda-4ba5-8de6-adbce301f9c6",
+      "date": "2024-01-03",
+      "city": "Debrecen",
+      "temperature": 3,
+      "rain": 17,
+      "wind": 26
+    },
+    {
+      "measurement_id": "f3db5060-76fa-4314-9bae-b104437f081a",
+      "date": "2024-01-09",
+      "city": "Budapest",
+      "temperature": 7,
+      "rain": 0,
+      "wind": 15
+    },
+    {
+      "measurement_id": "384c3754-bcb0-4ce6-87e3-14c67b65e7a2",
+      "date": "2024-01-28",
+      "city": "Budapest",
+      "temperature": 9,
+      "rain": 0,
+      "wind": 0
+    },
+    {
+      "measurement_id": "b81cc85c-fb10-4ede-b9ce-0eff2a5c03ce",
+      "date": "2024-01-09",
+      "city": "Szeged",
+      "temperature": 2,
+      "rain": 30,
+      "wind": 16
+    },
+    {
+      "measurement_id": "09bd19bc-c33a-41eb-b6a8-d662604dc81b",
+      "date": "2024-01-28",
+      "city": "Budapest",
+      "temperature": 1,
+      "rain": 11,
+      "wind": 17
+    },
+    {
+      "measurement_id": "90ddd22b-1310-4e59-bd5e-c79807bc24a1",
+      "date": "2024-01-30",
+      "city": "Budapest",
+      "temperature": 3,
+      "rain": 0,
+      "wind": 0
+    },
+    {
+      "measurement_id": "2e868bf8-bf19-4c00-aead-b3b3297ee335",
+      "date": "2024-01-01",
+      "city": "Budapest",
+      "temperature": 7,
+      "rain": 0,
+      "wind": 0
+    },
+    {
+      "measurement_id": "e6fea829-a0d7-4c59-a975-463ead4be833",
+      "date": "2024-01-30",
+      "city": "Budapest",
+      "temperature": 9,
+      "rain": 0,
+      "wind": 0
+    },
+    {
+      "measurement_id": "4b251f9e-a069-43b2-928d-4f761fcfc515",
+      "date": "2024-01-29",
+      "city": "Debrecen",
+      "temperature": 5,
+      "rain": 0,
+      "wind": 0
+    },
+    {
+      "measurement_id": "b7caa693-ff45-45a3-88c6-9ba7c00484e9",
+      "date": "2024-01-12",
+      "city": "Budapest",
+      "temperature": 10,
+      "rain": 0,
+      "wind": 4
+    },
+    {
+      "measurement_id": "1ad08d78-dfcd-425d-96a7-ed6b89a88750",
+      "date": "2024-01-06",
+      "city": "Budapest",
+      "temperature": 6,
+      "rain": 20,
+      "wind": 0
+    },
+    {
+      "measurement_id": "38e4b293-6ef0-4d5b-929c-98e1d4a7d930",
+      "date": "2024-01-13",
+      "city": "Budapest",
+      "temperature": 10,
+      "rain": 36,
+      "wind": 0
+    },
+    {
+      "measurement_id": "beafe144-3045-40d6-8bcd-9830ede196d5",
+      "date": "2024-01-27",
+      "city": "Budapest",
+      "temperature": 5,
+      "rain": 18,
+      "wind": 0
+    },
+    {
+      "measurement_id": "d0510e67-1949-451e-a3e8-3a9c47947d45",
+      "date": "2024-01-20",
+      "city": "Győr",
+      "temperature": 8,
+      "rain": 0,
+      "wind": 0
+    },
+    {
+      "measurement_id": "0e37b06e-dde6-41e7-8bd9-06a95d03f3ea",
+      "date": "2024-01-18",
+      "city": "Győr",
+      "temperature": 8,
+      "rain": 23,
+      "wind": 13
+    },
+    {
+      "measurement_id": "e187db68-e883-4ee8-8843-9eab70b2006c",
+      "date": "2024-01-20",
+      "city": "Debrecen",
+      "temperature": 7,
+      "rain": 0,
+      "wind": 38
+    },
+    {
+      "measurement_id": "e378dfc0-a2ab-42f4-b425-74a8032604b5",
+      "date": "2024-01-12",
+      "city": "Budapest",
+      "temperature": 7,
+      "rain": 0,
+      "wind": 0
+    },
+    {
+      "measurement_id": "8fc1fcd4-fefb-4b28-999f-a0cf5463ff74",
+      "date": "2024-01-28",
+      "city": "Szeged",
+      "temperature": 6,
+      "rain": 0,
+      "wind": 21
+    },
+    {
+      "measurement_id": "38a4714b-898e-4c28-b03f-62130b8b54b8",
+      "date": "2024-01-08",
+      "city": "Debrecen",
+      "temperature": 5,
+      "rain": 46,
+      "wind": 39
+    },
+    {
+      "measurement_id": "048b0eb1-a60a-4133-90c1-23f106e62de9",
+      "date": "2024-01-21",
+      "city": "Budapest",
+      "temperature": 6,
+      "rain": 0,
+      "wind": 11
+    },
+    {
+      "measurement_id": "da147ecb-a18c-4361-9e1f-6907e9b119a6",
+      "date": "2024-01-07",
+      "city": "Budapest",
+      "temperature": 1,
+      "rain": 0,
+      "wind": 0
+    },
+    {
+      "measurement_id": "23e045f4-32d8-4f37-aa70-e76983745461",
+      "date": "2024-01-30",
+      "city": "Debrecen",
+      "temperature": 3,
+      "rain": 0,
+      "wind": 0
+    },
+    {
+      "measurement_id": "01445877-1520-48b8-b8a8-ab0c6ef0b020",
+      "date": "2024-01-01",
+      "city": "Budapest",
+      "temperature": 8,
+      "rain": 0,
+      "wind": 0
+    },
+    {
+      "measurement_id": "3e3c026f-f34b-4df7-be32-4178760b732b",
+      "date": "2024-01-29",
+      "city": "Budapest",
+      "temperature": 4,
+      "rain": 27,
+      "wind": 0
+    },
+    {
+      "measurement_id": "2d2a133a-1cb0-4a15-8220-c8b7ab230750",
+      "date": "2024-01-24",
+      "city": "Szeged",
+      "temperature": 4,
+      "rain": 30,
+      "wind": 0
+    },
+    {
+      "measurement_id": "2016de6f-0928-46d9-a6a3-2c3ff70f256c",
+      "date": "2024-01-20",
+      "city": "Budapest",
+      "temperature": 3,
+      "rain": 0,
+      "wind": 0
+    },
+    {
+      "measurement_id": "d85f392a-1269-4344-83cd-2491d9009b61",
+      "date": "2024-01-22",
+      "city": "Budapest",
+      "temperature": 1,
+      "rain": 0,
+      "wind": 0
+    },
+    {
+      "measurement_id": "8814df22-2118-4dba-9aa3-499643c891e9",
+      "date": "2024-01-07",
+      "city": "Budapest",
+      "temperature": 1,
+      "rain": 0,
+      "wind": 0
+    },
+    {
+      "measurement_id": "661d0f51-e7cb-46fe-9c44-fe9080681471",
+      "date": "2024-01-06",
+      "city": "Győr",
+      "temperature": 5,
+      "rain": 13,
+      "wind": 0
+    },
+    {
+      "measurement_id": "a203e067-ae91-48e4-9c42-0087725efa9e",
+      "date": "2024-01-22",
+      "city": "Budapest",
+      "temperature": 10,
+      "rain": 0,
+      "wind": 0
+    },
+    {
+      "measurement_id": "434eaa4d-1b5a-481d-b45d-93f980349281",
+      "date": "2024-01-30",
+      "city": "Győr",
+      "temperature": 4,
+      "rain": 0,
+      "wind": 17
+    }
+];
